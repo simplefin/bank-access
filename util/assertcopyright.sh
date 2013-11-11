5f3e291f5eaa69211bb7bc37ee15abd01b7b5167
Matt Haggard <haggardii@gmail.com>
2013-11-11 08:18:38 -0700

    Simplify copyright test

--- util/assertcopyright.sh
+++ util/assertcopyright.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Exit with non-zero if there are files missing copyright.
 
-ALL_FILES=$(find . -regex '^.*\.\(py\|sh\|md\)' -regextype posix-extended -type f | cut -c3- | grep -v '/_' | sort)
+ALL_FILES=$(find . -type f | egrep '\.(py|sh|md)$' | cut -c3- | grep -v '/_' | sort)
 HAVE_COPYRIGHT=$(grep -rl "Copyright (c) The SimpleFIN Team" * | sort)
 MISSING=$(diff --unchanged-line-format= --old-line-format='%L' --new-line-format= <(echo "$ALL_FILES") <(echo "$HAVE_COPYRIGHT"))
 
