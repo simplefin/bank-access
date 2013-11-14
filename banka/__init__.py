62f5276c58676b13666aeccd969fcf813536a5fc
Matt Haggard <haggardii@gmail.com>
2013-11-13 19:47:18 -0700

    Fix make clean and copyright problems

---
+++ banka/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) The SimpleFIN Team
+# See LICENSE for details.
+
+__all__ = ['__version__']
+
+from banka.version import __version__
--- banka/version.py
+++ banka/version.py
@@ -1,1 +1,4 @@
+# Copyright (c) The SimpleFIN Team
+# See LICENSE for details.
+
 __version__ = "0.2.0-dev"
