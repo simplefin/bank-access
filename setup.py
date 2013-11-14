9c136b73a8a2995d8510592bfa5c6aee632323ff
Matt Haggard <haggardii@gmail.com>
2013-11-13 19:11:53 -0700

    Switch to semantic version and include more files in sdist

--- setup.py
+++ setup.py
@@ -3,13 +3,27 @@
 
 from distutils.core import setup
 
+import re
+import os
+
+def getVersion():
+    """
+    Get version from version file without importing.
+    """
+    r = re.compile(r'__version__ = "(.*?)"')
+    version_file = os.path.join(os.path.dirname(__file__), 'banka/version.py')
+    fh = open(version_file, 'rb')
+    for line in fh.readlines():
+        m = r.match(line)
+        if m:
+            return m.groups()[0]
 
 setup(
     url='none',
     author='Matt Haggard',
     author_email='matt@simplefin.org',
     name='banka',
-    version='0.1',
+    version=getVersion(),
     packages=[
         'banka', 'banka.test',
         'banka.ofx', 'banka.ofx.test',
